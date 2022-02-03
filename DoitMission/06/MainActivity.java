5f776fda47e12e2eb60f3a1ba5d4d7e7341deed9
Allgot <96973603+Allgot@users.noreply.github.com>
2022-02-03 14:27:13 +0900

    Create MainActivity.java

---
+++ DoitMission/06/MainActivity.java
@@ -0,0 +1,41 @@
+package dev_allgot.understand.doitmission_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    SeekBar seekBar;
+    EditText progressEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        seekBar = findViewById(R.id.seekBar);
+        progressEdit = findViewById(R.id.progressEdit);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress);
+                String message = Integer.toString(progress);
+                progressEdit.setText(message);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
