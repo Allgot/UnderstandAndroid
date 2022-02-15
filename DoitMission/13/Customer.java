eafdc68c56a6dc5d13f8ba0cea9317ab821cac62
Allgot <96973603+Allgot@users.noreply.github.com>
2022-02-15 15:32:00 +0900

    Create Customer.java

---
+++ DoitMission/13/Customer.java
@@ -0,0 +1,13 @@
+package dev_allgot.understand.doitmission_13;
+
+public class Customer {
+    public String name;
+    public String birth;
+    public String phone;
+
+    public Customer(String name, String birth, String phone) {
+        this.name = name;
+        this.birth = birth;
+        this.phone = phone;
+    }
+}
