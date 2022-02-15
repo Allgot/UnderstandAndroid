21603884d8470fa55d40ac27f8203d2349729ece
Allgot <96973603+Allgot@users.noreply.github.com>
2022-02-15 17:16:22 +0900

    Create OnProductTouchListener.java

---
+++ DoitMission/14/OnProductTouchListener.java
@@ -0,0 +1,7 @@
+package dev_allgot.understand.doitmission_14;
+
+import android.view.View;
+
+public interface OnProductTouchListener {
+    void onProductTouch(ProductAdapter.ViewHolder holder, View view, int position);
+}
