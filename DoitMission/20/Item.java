5982f104b6325530daca6b7ea05e8717cdb60cbc
Allgot <96973603+Allgot@users.noreply.github.com>
2022-02-19 01:22:52 +0900

    Create Item.java

---
+++ DoitMission/20/Item.java
@@ -0,0 +1,11 @@
+package dev_allgot.understand.doitmission_20;
+
+public class Item {
+    String title;
+    String description;
+
+    public Item(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
