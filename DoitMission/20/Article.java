c153854da3974db6acc5dc85a5d8c36e02716ae2
Allgot <96973603+Allgot@users.noreply.github.com>
2022-02-18 23:59:20 +0900

    Create Article.java

---
+++ DoitMission/20/Article.java
@@ -0,0 +1,11 @@
+package dev_allgot.understand.doitmission_20;
+
+public class Article {
+    public String title;
+    public String description;
+
+    public Article(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
