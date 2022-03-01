e2e901758cc81d022b638412170edc50fccfeea0
Allgot <96973603+Allgot@users.noreply.github.com>
2022-03-01 17:58:19 +0900

    Create friend.java

---
+++ DoitMission/29/friend.java
@@ -0,0 +1,11 @@
+package dev_allgot.understand.doitmission_29;
+
+public class Friend {
+    public String name;
+    public String phone;
+
+    public Friend(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
